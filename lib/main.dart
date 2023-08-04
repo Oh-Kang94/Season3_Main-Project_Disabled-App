c8888f885e7ee8f79361500d4a657f8bff8ddd49
Oh-Kang94 <okh19941994@naver.com>
2023-08-04 21:54:59 +0900

    hide .env
    commit #008

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:season3_team1_mainproject/view/splash.dart';
 
 import 'firebase_options.dart';
-import 'view/home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-
+  await dotenv.load(); // .env 파일 로드
   runApp(const MyApp());
 }
 
