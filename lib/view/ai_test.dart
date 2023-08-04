3aa33d6b6cb41737edae449088404fcfa6c84273
Jty98 <clclcfnfn@naver.com>
2023-08-04 17:42:55 +0900

    ai테스트 화면 생성

---
+++ lib/view/ai_test.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AiTest extends StatefulWidget {
+  const AiTest({super.key});
+
+  @override
+  State<AiTest> createState() => _AiTestState();
+}
+
+class _AiTestState extends State<AiTest> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
