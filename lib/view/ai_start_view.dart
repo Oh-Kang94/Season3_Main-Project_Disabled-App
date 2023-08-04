6502e76ac87c0417632c61bca4754fcd60cb9ada
Jty98 <clclcfnfn@naver.com>
2023-08-04 18:24:01 +0900

    ai 시작 화면과 테스트 화면 추가

---
+++ lib/view/ai_start_view.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
+import 'package:season3_team1_mainproject/view/ai_test_view.dart';
+
+import '../util/asset_image.dart';
+
+class AiFirstView extends StatelessWidget {
+  const AiFirstView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () => Get.to(AiTestView()),
+              child: SvgPicture.asset(
+                AssetsImage.LOGO,
+                width: 300,
+                height: 300,
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const Text('버튼을 눌러서 테스트를 시작'),
+            Icon(Icons.recommend),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/ai_test_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AiTestView extends StatefulWidget {
+  const AiTestView({super.key});
+
+  @override
+  State<AiTestView> createState() => _AiTestViewState();
+}
+
+class _AiTestViewState extends State<AiTestView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
