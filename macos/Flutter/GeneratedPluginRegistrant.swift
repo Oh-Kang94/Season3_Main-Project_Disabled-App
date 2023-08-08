f9c184b5c7df4d5a2337b426cd894d8d7bd399f5
ms k <msk@msui-MacBookPro.local>
2023-08-08 16:54:04 +0900

    intl yaml 수정 및 geolocator 추가. 그 외 pubget시 변경되는 항목들 같이 추가하여 staged함

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,7 @@ import file_selector_macos
 import firebase_core
 import firebase_storage
 import flutter_secure_storage_macos
-import location
+import geolocator_apple
 import path_provider_foundation
 import shared_preferences_foundation
 
@@ -20,7 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
-  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
