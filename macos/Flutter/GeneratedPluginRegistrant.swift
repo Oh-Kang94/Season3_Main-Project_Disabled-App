3ca211b2b1250ab0127571fb74c5d7704d777ab7
ms k <msk@msui-MacBookPro.local>
2023-08-08 14:08:03 +0900

    map관련 yaml추가
      # Flutter용 Google 지도 위젯을 제공하는 Flutter 플러그인
      google_maps_flutter: ^2.4.0
      #구글 맵 api를 직접 사용하기위해 사용하는 패키지
      google_maps_flutter_web: ^0.5.3
      http: ^1.1.0
      #주소값 좌표로 좌표값 주소로
      geocoding: ^2.1.0
      #현재위치
      location: ^5.0.2+1

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import file_selector_macos
 import firebase_core
 import firebase_storage
 import flutter_secure_storage_macos
+import location
 import path_provider_foundation
 import shared_preferences_foundation
 
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
