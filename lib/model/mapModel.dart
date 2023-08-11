01e62b19357372b231749e73251f033d9892e022
ms k <msk@msui-MacBookPro.local>
2023-08-11 23:02:51 +0900

    mapModel factory 형식으로 수정

--- lib/model/mapModel.dart
+++ lib/model/mapModel.dart
@@ -1,4 +1,15 @@
 class MapModel{
+  late final int id;
+  late final String capacity;
+  late final String city;
+  late final int isVirtualstation;
+  late final double latitude;
+  late final double longitude;
+
+  late final String name;
+  late final String address;
+  late final String seq;
+  // late final int zip;
   MapModel({
     required this.id,
     required this.capacity,
@@ -12,46 +23,34 @@ class MapModel{
     required this.seq,//seq
     // required this.zip,
   });
-  late final int id;
-  late final String capacity;
-  late final String city;
-  late final int isVirtualstation;
-  late final double latitude;
-  late final double longitude;
+  
 
-  late final String name;
-  late final String address;
-  late final String seq;
-  // late final int zip;
-
-  MapModel.fromJson(Map<String, dynamic> json){
-    id = json['id'];
-    capacity = json['capacity'];
-    city = json['city'];
-    isVirtualstation = json['isVirtualstation'];
-    latitude = json['latitude'];
-    longitude = json['longitude'];
-
-    name = json['name'];
-    address = json['address'];
-    seq = json['seq'];
+  factory MapModel.fromJson(Map<String, dynamic> json){
+    return MapModel(
+      id: json['id'], 
+      capacity: json['capacity'], 
+      city: json['city'], 
+      isVirtualstation: json['isVirtualstation'], 
+      latitude: json['latitude'], 
+      longitude: json['longitude'], 
+      name: json['name'], 
+      address: json['address'], 
+      seq: json['seq']);
     // zip = json['zip'];
   }
 
   Map<String, dynamic> toJson(){
-    final _data = <String, dynamic>{};
-    _data['id']= id;
-    _data['capacity']= capacity;
-    _data['city']= city;
-    _data['isVirtualstation']= isVirtualstation;
-    _data['latitude']= latitude;
-    _data['longitude']= longitude;
-
-    _data['name']= name;
-    _data['address']= address;
-    _data['seq']=seq;    
-    // _data['zip']= zip;
-    return _data;
+    return{
+      'id': id,
+      'capacity':capacity,
+      'city': city,
+      'isVirtualstation':isVirtualstation,
+      'latitude':latitude,
+      'longitude':longitude,
+      'name':name,
+      'address':address,
+      'seq':seq
+    };
   }
 
   
