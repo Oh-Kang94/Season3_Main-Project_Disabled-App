92314c3f025a8a1db8d41f4520c232d4bff2f451
ms k <msk@msui-MacBookPro.local>
2023-08-11 23:20:33 +0900

    mapView, mapcontroller중간점검
    현재 필요한 정보: mapController수정 필요 -> 지오코딩 후 변환된 좌표를 dto에 저장시키는 과정 필요한 것으로 생각중
    그리고 그 후 mapView에서 저장된 좌표값들 불러와서 맵에 마킹시키면 될 것으로 생각

--- lib/view/mapView.dart
+++ lib/view/mapView.dart
@@ -11,9 +11,9 @@
 //         title: const Text('Map'),
 //       ),
 //       body: const Center(
-//         child: 
+//         child:
 //         // Obx(() => mapController.mapModel.isNotEmpty
-//             // ? 
+//             // ?
 //             SizedBox(
 //               height: 600,
 //               width: 200,
@@ -29,47 +29,99 @@
 //                   ),
 //                 ),
 //             ),
-              
+
 //             // : Center(
 //                 // child: CircularProgressIndicator(),
 //               // ),
 //               // ),
-              
+
+//       ),
+//     );
+//   }
+// }
+
+// import 'package:flutter/material.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+// import '../vm/map_controller.dart';
+
+// class mapView extends StatefulWidget {
+//   mapView({super.key});
+
+//   MapController _mapController = MapController();
+
+//   @override
+//   State<mapView> createState() => _mapViewState();
+// }
+
+// class _mapViewState extends State<mapView> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Map'),
+
+//       ),
+//       body: const Center(
+//         child: SizedBox(
+//           height: 600,
+//           width: 400,
+//           child: GoogleMap(
+//             mapType: MapType.normal,
+//             initialCameraPosition: CameraPosition(
+//               target: LatLng(37.494552, 127.029932),
+//               zoom: 18,
+//             ),
+//           ),
+//         ),
 //       ),
 //     );
 //   }
 // }
 
 
+//stateful로 다시 코드 짜기
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:flutter/gestures.dart';
 
-class mapView extends StatelessWidget {
-  const mapView({super.key});
+import '../vm/map_controller.dart';
+
+class MapViewPage extends StatefulWidget {
+  @override
+  _MapViewPageState createState() => _MapViewPageState();
+}
+
+class _MapViewPageState extends State<MapViewPage> {
+  final MapController _mapController = MapController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadLocationData();
+  }
+
+  Future<void> _loadLocationData() async {
+    await _mapController.fetchLocationData();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Map'),
+        title: Text('Map View'),
       ),
-      body: IgnorePointer(
-        ignoring: true,
-
-        child: const Center(
-          child: SizedBox(
-            height: 600,
-            width: 400,
-            child: GoogleMap(
-              mapType: MapType.normal,
-              initialCameraPosition: CameraPosition(
-                target: LatLng(37.494552, 127.029932),
-                zoom: 18,
-              ),
-            ),
-          ),
-        ),
+      body: Center(
+        child: _mapController.markers.isNotEmpty
+            ? GoogleMap(
+                mapType: MapType.normal,
+                initialCameraPosition: CameraPosition(
+                  target: LatLng(37.494552, 127.029932),
+                  zoom: 18,
+                ),
+                markers: _mapController.markers.toSet(),
+              )
+            : CircularProgressIndicator(), // 데이터 로딩 중에는 로딩 인디케이터 표시
       ),
     );
   }
--- lib/vm/map_controller.dart
+++ lib/vm/map_controller.dart
@@ -13,34 +13,34 @@ class MapController extends GetxController{
   List<MapModel> mapModel = <MapModel>[].obs;//mapmodel을 가져올 것
   var markers = RxSet<Marker>(); //map에 marker찍어줄 변수
   RxBool isLoading = false.obs;//완료되었는지 여부 bool함수
-  var Url =  Uri.tryParse('');
+  var Url =  Uri.tryParse('http://www.oh-kang.kro.kr:7288/maps/');
 
   //api url제공하는 함수
     //결과를 얻은 뒤에 문자열 맵목록으로 변환 시킨다.
     //그 후 다시 모델에 추가하는 과정을 거치는 함수
-  fetchLocations() async{
-    try{
-      isLoading(true);
-      http.Response response = await http.get(Url!);
-      if(response.statusCode ==200){
-        var result = jsonDecode(response.body);
-        log(result.toString());
-        mapModel.addAll(RxList<Map<String,dynamic>>.from(result)
-          .map((e) => MapModel.fromJson(e))
-          .toList());
-      }
-    } catch(e){
-      print('Error while getting data is $e');
-    } finally{
-      isLoading(false);
-      print('finally: $mapModel');
-      createMarkers();//마커를 생성하는 함수
-    }
-  }
+  // fetchLocations() async{
+  //   try{
+  //     isLoading(true);
+  //     http.Response response = await http.get(Url!);
+  //     if(response.statusCode ==200){
+  //       var result = jsonDecode(response.body);
+  //       log(result.toString());
+  //       mapModel.addAll(RxList<Map<String,dynamic>>.from(result)
+  //         .map((e) => MapModel.fromJson(e))
+  //         .toList());
+  //     }
+  //   } catch(e){
+  //     print('Error while getting data is $e');
+  //   } finally{
+  //     isLoading(false);
+  //     print('finally: $mapModel');
+  //     createMarkers();//마커를 생성하는 함수
+  //   }
+  // }
   //완료되면 로딩을 거짓으로 만든 뒤 api에서 가져온 위치에 마커를 생성해야 한다.
 
 
-//location받아오기
+//location받아오기. awsdb로 부터
 Future<void> fetchLocationData() async {
   final url = Uri.parse('http://www.oh-kang.kro.kr:7288/maps/');
   
@@ -54,6 +54,7 @@ Future<void> fetchLocationData() async {
         final int seq = location['seq'];
         final String businessName = location['사업장명'];
         final String address = location['주소'];
+        // 추후 지오코딩 후 반영 예정.
         // final double latitude = location['위도']; // 위도 정보 추가
         // final double longitude = location['경도']; // 경도 정보 추가
 
@@ -61,12 +62,15 @@ Future<void> fetchLocationData() async {
         markers.add(Marker(
           markerId: MarkerId(seq.toString()),
           icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-          position: LatLng(37.494552, 127.029932),
+          position: LatLng(37.494552, 127.029932),//일단 학원위치로 고정좌표 찍어놓음
           infoWindow: InfoWindow(title: businessName, snippet: address),
           onTap: () {
             print('마커 탭됨: $businessName');
           },
         ));
+        print(address);
+        //지오코딩하자
+        
       }
     } else {
       print('Request failed with status: ${response.statusCode}');
@@ -77,19 +81,19 @@ Future<void> fetchLocationData() async {
 }
 
 
-  //마커 생성함수
-  createMarkers(){
-    mapModel.forEach((list) {
-      markers.add(Marker(
-        markerId: MarkerId(list.id.toString()),  
-        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
-        position: LatLng(list.latitude, list.longitude),
-        infoWindow: InfoWindow(title: list.name, snippet: list.city),
-        onTap: () {
-          print('market tapped');
-        },
+  //마커 생성함수 (기본적인 마커 생성함수 우리가 쓸건 아니므로 일단 제외함.)
+  // createMarkers(){
+  //   mapModel.forEach((list) {
+  //     markers.add(Marker(
+  //       markerId: MarkerId(list.id.toString()),  
+  //       icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+  //       position: LatLng(list.latitude, list.longitude),
+  //       infoWindow: InfoWindow(title: list.name, snippet: list.city),
+  //       onTap: () {
+  //         print('market tapped');
+  //       },
 
-      ),);
-    });
-  }
+  //     ),);
+  //   });
+  // }
 }
