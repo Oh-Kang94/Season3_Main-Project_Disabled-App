7942660f1855b275b439c6b4067908d1153c8eb4
Jty98 <clclcfnfn@naver.com>
2023-08-13 21:17:08 +0900

    yaml 추가 및 chartWidget 만들기

---
+++ lib/components/ai_result_chart_widget.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AiResultChartWidget extends StatelessWidget {
+  const AiResultChartWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      
+    );
+  }
+}
